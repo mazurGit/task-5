6939eb758d2c9e851da4f0393453b6a41a475b53
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-06-13 10:02:41 +0300

    signUp form control and validation added

--- src/components/pages/signUp/SignUp.jsx
+++ src/components/pages/signUp/SignUp.jsx
@@ -1,6 +1,35 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const SignUp = () =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+    const  onInputChange = (e)=> {
+        const value = e.target.value
+        const name =e.target.name
+        switch(name){
+            case 'password':
+                setPassword(value);
+                break;
+            case 'email':
+                setEmail(value)
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    const onBlur = (e) => {
+        const target = e.target
+        if (target.value.length < 3 ) {
+            target.style.boxShadow = '0 0 2px 1px red'
+        } else{
+            target.style.boxShadow = ''
+        }
+    }
+
     return (
         <main className="sign-up-page">
             <h1 className="visually-hidden">Travel App</h1>
@@ -8,15 +37,36 @@ const SignUp = () =>{
             <h2 className="sign-up-form__title">Sign Up</h2>
             <label className="trip-popup__input input">
                 <span className="input__heading">Full name</span>
-                <input name="full-name" type="text" required />
+                <input 
+                    name="full-name" 
+                    type="text" 
+                    required 
+                />
             </label>
             <label className="trip-popup__input input">
                 <span className="input__heading">Email</span>
-                <input name="email" type="email" required />
+                <input 
+                    name="email" 
+                    type="email" 
+                    value={email}
+                    onChange={onInputChange} 
+                    title="myemail@email.com" 
+                    required 
+                />
             </label>
             <label className="trip-popup__input input">
                 <span className="input__heading">Password</span>
-                <input name="password" type="password" autoComplete="new-password" required />
+                <input 
+                    name="password" 
+                    type="password" 
+                    autoComplete="new-password" 
+                    minLength={3}
+                    value={password}
+                    onChange={onInputChange} 
+                    onBlur={onBlur} 
+                    title = "Minimum 3 characters"
+                    required 
+                />
             </label>
             <button className="button" type="submit">Sign Up</button>
             </form>
