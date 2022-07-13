4d82a5b52138763c4522638f6d17b38e07b5c7ec
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-07-14 00:46:26 +0300

    http requests services was created

---
+++ src/helpers/localStorage.js
@@ -0,0 +1,11 @@
+export const storageSetToken = (token) =>{
+    localStorage.setItem('token', token)
+}
+
+export const storageGetToken = () => {
+    return localStorage.getItem('token')
+}
+
+export const storegeRemoveToken = () =>{
+    return localStorage.removeItem('token');
+}
