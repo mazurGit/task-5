9beff1cd8389c9fd0688cb6e03d2ba686d399e14
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-07-14 01:19:55 +0300

    main page logic done and added comp: Erro, Spinner, Boundary

---
+++ src/hooks/verifyUser.hook.js
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { storageGetToken } from '../helpers/localStorage';
+import { verifyUser } from '../store/actions/user';
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
+
+
+export const useVerifyUserHook = (navLink) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const user = useSelector(state => state.user.userData)
+
+    const verify = useCallback( async() => {
+        if (storageGetToken() && !user ){
+                dispatch(verifyUser())
+        } else {
+            navigate(navLink)
+        }
+    },[user])
+    return {verify}
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { user } from "./reducers/user";
+import { trips } from "./reducers/trips";
 import service from "../helpers/requests";
 
 const store = configureStore({
     reducer:{
-        user
+        user,
+        trips
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware({
         thunk: {extraArgument:service}
